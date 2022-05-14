ba613b21854033328e91ae689a104ae31ca34255
OthmanAdi <github_me@outlook.de>
2022-05-14 11:46:48 +0200

    version2

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
-const container = document.querySelector(".container");
+const bck = document.querySelector(".container");
 
-const randomColor()
-
-const paint()
+const randomColor = () => {
+    let color = 'rgba(';
+    for(let start = 0;start< 3;start++){
+        color += Math.floor(Math.random() * 255) + ',';
+    }
+    return color;
+}
